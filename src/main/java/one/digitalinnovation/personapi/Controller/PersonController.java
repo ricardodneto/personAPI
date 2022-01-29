c823e26cdac747dadc26cb146895ce221e528309
ricardodneto <ricardoz.s474@gmail.com>
2022-01-29 01:53:25 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/personapi/Controller/PersonController.java
@@ -0,0 +1,36 @@
+package one.digitalinnovation.personapi.Controller;
+
+import javax.validation.Valid;
+
+import one.digitalinnovation.personapi.dto.Response.MessageResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.Response.MessageResponseDTO;
+import one.digitalinnovation.personapi.service.PersonService;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    private PersonService personService;
+
+    @Autowired(required = true)
+    public PersonController(PersonService personService) {
+        super();
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
+    }
+
+}
---
+++ src/main/java/one/digitalinnovation/personapi/Entity/Phone.java
@@ -0,0 +1,36 @@
+package one.digitalinnovation.personapi.Entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.Enums.PhoneType;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+}
